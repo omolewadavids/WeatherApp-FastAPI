d600910c4d9d672d8ca74d26a7ce67933583da2f
Adaramola Omolewa <omolewa.davids@gmail.com>
2025-03-18 22:00:14 -0400

    code updates

--- backend/app/api/v1/endpoints/weather.py
+++ backend/app/api/v1/endpoints/weather.py
@@ -1,6 +1,6 @@
-import json
 
-from fastapi import FastAPI, APIRouter, Body, Depends, HTTPException, Path, File, UploadFile
+
+from fastapi import APIRouter, Depends, HTTPException
 
 from starlette.status import (
     HTTP_200_OK,
@@ -13,9 +13,11 @@ from starlette.status import (
 
 from fastapi.security import OAuth2PasswordRequestForm
 
-from backend.app.models.weather import WeatherResponse
+from backend.app.models.weather import WeatherResponse, WeatherPredictionResponse
 from backend.app.db.repositories.weather import WeatherRepository
-from backend.app.api.v1.dependencies.database import get_repository
+from backend.app.api.v1.dependencies.database import get_repository, get_api_key
+from backend.app.utils.ai_model import predict_temperature_trend
+
 
 router = APIRouter()
 
@@ -32,3 +34,15 @@ async def get_weather_by_city(
         raise HTTPException(status_code=404, detail="City not found")
 
     return info
+
+
+@router.get("/predict_temperature/{city}", response_model=WeatherPredictionResponse)
+async def predict_temperature(query: str,
+                              city: str,
+                              api_key: str = Depends(get_api_key),
+                              weather_repo: WeatherRepository = Depends(get_repository(WeatherRepository))):
+    weather_data = await weather_repo.get_weather_by_city(city=city, query=query)
+    current_temp = weather_data["main"]["temp"]
+    predicted_temp = predict_temperature_trend(current_temp)
+    return WeatherPredictionResponse(predicted_temperature=predicted_temp)
+
